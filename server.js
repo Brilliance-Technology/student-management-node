b28d293448e302cde931b2fcceb8189be13382a4
Brilliance-Technology <dheeraj@brilliance.co.in>
2021-11-26 16:43:15 +0530

    adding heroku file

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express=require('express');
 const app= express();
+
+let port = process.env.PORT ||3000;
 require('./db_connection');
 app.use(express.json());
 const bodyParser = require('body-parser');
@@ -10,8 +12,6 @@ app.use(cors());
 const StudentUser=require('./routers/student')
 app.use('/student',StudentUser);
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
+server.listen(port, ()=> {
     console.log('Listening on port %d', server_port);
 });
