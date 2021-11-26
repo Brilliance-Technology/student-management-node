ba0f42c99f83b694988e9fa4e9238d30492772a7
Brilliance-Technology <dheeraj@brilliance.co.in>
2021-11-26 16:34:39 +0530

    change server port

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.use(cors());
 const StudentUser=require('./routers/student')
 app.use('/student',StudentUser);
 
-app.listen(5000,() =>{
-    console.log(" Server Listen at Port 5000");
-
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
 });
